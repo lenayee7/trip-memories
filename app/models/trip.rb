4aae0abfa43d24ca314627f87190f9a247fd8685
lenayee7 <lenayee07@gmail.com>
2016-05-21 22:51:41 -0700

    trying to fix paperclip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,5 @@ class Trip < ActiveRecord::Base
 	validates_presence_of :title
 	belongs_to :user
 	has_many :posts, dependent: :destroy
-  has_attached_file :cover_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/suncloud.jpg"
-  validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\Z/
-end
+  has_attached_file :cover_image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :cover_image, content_type: "image/jpeg", "image/gif", "image/png"]
