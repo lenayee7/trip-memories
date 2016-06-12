19315744d40b014989c0fa808d00b8908904f32f
lenayee7 <lenayee07@gmail.com>
2016-06-11 20:00:19 -0700

    adds auto orient

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	validates :title, presence: true
   belongs_to :trip
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "https://s3.amazonaws.com/trip-memories/jusgotravel.jpg"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/, :convert_options => { :all => '-auto-orient' }
 
 end
